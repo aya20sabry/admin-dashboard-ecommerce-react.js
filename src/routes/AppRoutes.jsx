f0f7b14f296d8d05d7725beb7dcf48cbb7ecc0aa
aya <ayasabry551999@gmail.com>
2025-10-07 14:13:49 -0700

    update navbar

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,10 +3,18 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import Layout from "../components/layout/Layout";
 
-// Dashboard Pages
+// Pages
 import Overview from "../pages/Overview/Overview";
+import Bag from "../pages/Product/Bag";
+import Jacket from "../pages/Product/Jacket";
+import Sneakers from "../pages/Product/Sneakers";
+import TShirt from "../pages/Product/TShirt";
+import SalesReport from "../pages/Reports/SalesReport";
 import Users from "../pages/Users/Users";
 import Settings from "../pages/Settings/Settings";
+import Transaction from "../pages/Transaction/Transaction";
+
+
 
 const AppRoutes = () => {
   return (
@@ -37,6 +45,52 @@ const AppRoutes = () => {
           </Layout>
         }
       />
+        <Route
+        path="/product/bag"
+        element={
+          <Layout>
+            <Bag />
+          </Layout>
+        }
+      />  <Route
+      path="/product/tshirt"
+      element={
+        <Layout>
+          <TShirt />
+        </Layout>
+      }
+    />  <Route
+    path="/product/jacket"
+    element={
+      <Layout>
+        <Jacket />
+      </Layout>
+    }
+  /> 
+   <Route
+  path="/product/sneakers"
+  element={
+    <Layout>
+      <Sneakers />
+    </Layout>
+  }
+/>
+<Route
+  path="/sales-report"
+  element={
+    <Layout>
+      <SalesReport />
+    </Layout>
+  }
+/>  
+ <Route
+  path="/transaction"
+  element={
+    <Layout>
+      <Transaction />
+    </Layout>
+  }
+/>
 
       {/* أي Route مش موجود يرجع للصفحة الرئيسية */}
       <Route path="*" element={<Navigate to="/" replace />} />
